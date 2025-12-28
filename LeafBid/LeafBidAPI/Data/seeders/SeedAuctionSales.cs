d0d0d32451db48b0035f9704b8cf3fd0f0d9d32b
Waylo2016 <wellinkstijn@gmail.com>
2025-12-28 10:12:44 +0100

    Add seeder for AuctionSales data
    Introduces SeedAuctionSales class to populate the AuctionSales table with initial data, linking auctions and users by specific criteria. This helps set up test or development environments with sample auction sale records.

---
+++ LeafBid/LeafBidAPI/Data/seeders/SeedAuctionSales.cs
@@ -0,0 +1,46 @@
+using LeafBidAPI.Enums;
+using LeafBidAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeafBidAPI.Data.seeders;
+
+public class SeedAuctionSales
+{
+    public static async Task SeedAuctionSalesAsync(ApplicationDbContext context, CancellationToken cancellationToken)
+    {
+        if (await context.Products.AnyAsync(cancellationToken))
+            return;
+
+        var AuctioneerID = await context.Users
+            .Where(u => u.UserName == "Auctioneer")
+            .Select(u => u.Id)
+            .FirstAsync(cancellationToken: cancellationToken);
+        
+        context.AuctionSales.AddRange(
+            new AuctionSale
+            {
+                AuctionId = await context.Auctions
+                    .Where(a => a.ClockLocationEnum == ClockLocationEnum.Aalsmeer && a.UserId == AuctioneerID)
+                    .Select(a => a.Id)
+                    .FirstAsync(cancellationToken: cancellationToken),
+                UserId = await context.Users
+                    .Where(u => u.UserName == "Buyer")
+                    .Select(u => u.Id)
+                    .FirstAsync(cancellationToken: cancellationToken),
+                PaymentReference = "1234567890"
+            },
+            new AuctionSale
+            {
+                AuctionId = await context.Auctions
+                    .Where(a => a.ClockLocationEnum == ClockLocationEnum.Naaldwijk && a.UserId == AuctioneerID)
+                    .Select(a => a.Id)
+                    .FirstAsync(cancellationToken: cancellationToken),
+                UserId = await context.Users
+                    .Where(u => u.UserName == "Buyer")
+                    .Select(u => u.Id)
+                    .FirstAsync(cancellationToken: cancellationToken),
+                PaymentReference = "0987654321"
+            }
+            );
+    }
+}
