d96e4c5a2fa06ed43ae6fd9f67b8f470bf86d451
Stijn Wellink <wellinkstijn@gmail.com>
2026-01-02 16:32:25 +0100

    Add GetAuctionPerActiveClockLocationDto class
    Introduces a DTO for returning a list of auctions with products, grouped by active clock location.

---
+++ LeafBid/LeafBidAPI/DTOs/Page/GetAuctionPerActiveClockLocationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace LeafBidAPI.DTOs.Page;
+
+using Models;
+
+public class GetAuctionPerActiveClockLocationDto
+{
+    public required List<GetAuctionWithProductsDto> Auctions { get; set; }
+}
