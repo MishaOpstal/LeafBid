7d5d4c240b2f0121afba36b610517522671f89e8
Stijn Wellink <wellinkstijn@gmail.com>
2026-01-02 16:32:40 +0100

    Add initial PagesService unit test class
    Introduces PagesServiceTest with setup for in-memory database and mocks. Prepares test data for auctions and products, laying groundwork for testing PagesServices functionality.

---
+++ LeafBid/LeafBidAPITest/Services/PagesServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using LeafBidAPI.Data;
+using LeafBidAPI.Models;
+using LeafBidAPI.Services;
+using LeafBidAPITest.Helpers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace LeafBidAPITest.Services;
+
+public class PagesServiceTest
+{
+    private readonly DbContextOptions<ApplicationDbContext> _dbOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+        .UseInMemoryDatabase(Guid.NewGuid().ToString())
+        .Options;
+    private readonly Mock<UserManager<User>> _userManagerMock = DummyUsers.CreateUserManagerMock();
+    
+    [Fact]
+    public async Task GetAuctionPerActiveClockLocation_ReturnsAuctionWithProductsDto_Successfully()
+    {
+        // Arrange (create auction and products)
+        await using ApplicationDbContext context = new(_dbOptions);
+        List<Auction> auctionList = DummyAuctions.GetFakeAuctions();
+        context.Auctions.AddRange(auctionList);
+        await context.SaveChangesAsync();
+        
+        List<RegisteredProduct> productList = DummyRegisteredProducts.GetFakeRegisteredProducts();
+        context.RegisteredProducts.AddRange(productList);
+        await context.SaveChangesAsync();
+        
+        
+        
+        PagesServices pagesServices = new PagesServices(context, _userManagerMock.Object);
+    }
+}
