d8125fc4834890967758d4bdb8d483d51b6ef81b
Stijn Wellink <wellinkstijn@gmail.com>
2026-01-02 16:31:08 +0100

    Create DummyAuctionProducts.cs
    introduce helper for creating dummy auction products

---
+++ LeafBid/LeafBidAPITest/Helpers/DummyAuctionProducts.cs
@@ -0,0 +1,139 @@
+﻿using LeafBidAPI.Models;
+
+namespace LeafBidAPITest.Helpers;
+
+public class DummyAuctionProducts
+{
+    public static List<AuctionProduct>
+    {
+        return
+        [
+            new AuctionProduct
+            {
+                AuctionId = 1,
+                RegisteredProductId = 1,
+                ServeOrder = 1,
+                AuctionStock = 5
+            },
+            
+            new AuctionProduct
+            {
+                AuctionId = 1,
+                RegisteredProductId = 2,
+                ServeOrder = 2,
+                AuctionStock = 3
+            },
+            
+            new AuctionProduct
+            {
+                AuctionId = 1,
+                RegisteredProductId = 3,
+                ServeOrder = 3,
+                AuctionStock = 10
+            },
+            
+            new AuctionProduct
+            {
+                AuctionId = 1,
+                RegisteredProductId = 4,
+                ServeOrder = 10,
+                AuctionStock = 15
+            },
+
+            new AuctionProduct
+            {
+                AuctionId = 2,
+                RegisteredProductId = 1,
+                ServeOrder = 1,
+                AuctionStock = 5
+            },
+            
+            new AuctionProduct
+            {
+                AuctionId = 2,
+                RegisteredProductId = 2,
+                ServeOrder = 2,
+                AuctionStock = 3
+            },
+            new AuctionProduct
+            {
+                AuctionId = 2,
+                RegisteredProductId = 3,
+                ServeOrder = 3,
+                AuctionStock = 10
+            },
+            
+            new AuctionProduct
+            {
+                AuctionId = 2,
+                RegisteredProductId = 4,
+                ServeOrder = 10,
+                AuctionStock = 15
+            },
+            
+            new AuctionProduct
+            {
+                AuctionId = 3,
+                RegisteredProductId = 1,
+                ServeOrder = 1,
+                AuctionStock = 5
+            },
+            
+            new AuctionProduct
+            {
+                AuctionId = 3,
+                RegisteredProductId = 2,
+                ServeOrder = 2,
+                AuctionStock = 3
+            },
+            
+            new AuctionProduct
+            {
+                AuctionId = 3,
+                RegisteredProductId = 3,
+                ServeOrder = 3,
+                AuctionStock = 10
+            },
+            
+            new AuctionProduct
+            {
+                AuctionId = 3,
+                RegisteredProductId = 4,
+                ServeOrder = 10,
+                AuctionStock = 15
+            },
+            
+            new AuctionProduct
+            {
+                AuctionId = 4,
+                RegisteredProductId = 1,
+                ServeOrder = 1,
+                AuctionStock = 5
+            },
+            
+            new AuctionProduct
+            {
+                AuctionId = 4,
+                RegisteredProductId = 2,
+                ServeOrder = 2,
+                AuctionStock = 3
+            },
+            
+            new AuctionProduct
+            {
+                AuctionId = 4,
+                RegisteredProductId = 3,
+                ServeOrder = 3,
+                AuctionStock = 10
+            },
+            
+            new AuctionProduct
+            {
+                AuctionId = 4,
+                RegisteredProductId = 4,
+                ServeOrder = 10,
+                AuctionStock = 15
+            }
+        ];
+    }
+}
