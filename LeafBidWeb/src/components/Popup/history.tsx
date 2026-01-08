d923052960aa406382b6ca266a4864f566367b5c
Robin <robinrid6@gmail.com>
2026-01-08 10:57:21 +0100

    fixed layout and removed legacy fix

--- LeafBidWeb/src/components/Popup/history.tsx
+++ LeafBidWeb/src/components/Popup/history.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { useState } from "react";
+import {useState} from "react";
 import Button from "react-bootstrap/Button";
-import { Table } from "react-bootstrap";
+import {Table} from "react-bootstrap";
 import s from "./history.module.css";
 import Image from "next/image";
 
@@ -12,6 +12,7 @@ type HistoryProps = {
     Picture: string;
     companyName: string;
 }
+
 // Een enkele sale item
 interface Sale {
     quantity: number;
@@ -47,14 +48,14 @@ export default function History(HistoryProps: HistoryProps) {
         try {
             const resCompany = await fetch(
                 `http://localhost:5001/api/v2/AuctionSaleProduct/history/${HistoryProps.RegisteredProductID}/company`,
-                { method: "GET", credentials: "include" }
+                {method: "GET", credentials: "include"}
             );
             const company = await resCompany.json();
             setCompanyData(company);
 
             const resNotCompany = await fetch(
                 `http://localhost:5001/api/v2/AuctionSaleProduct/history/${HistoryProps.RegisteredProductID}/not-company`,
-                { method: "GET", credentials: "include" }
+                {method: "GET", credentials: "include"}
             );
             const notCompany = await resNotCompany.json();
             setNotCompanyData(notCompany);
@@ -76,49 +77,58 @@ export default function History(HistoryProps: HistoryProps) {
 
             <div id="history" className={s.popover} popover={"auto"}>
                 <div className={s.productInfo}>
-                    <Image src={HistoryProps.Picture} alt={HistoryProps.Name} width={50} height={50} />
+                    <Image src={HistoryProps.Picture} alt={HistoryProps.Name} width={50} height={50}/>
                     <h3>{HistoryProps.Name}</h3>
 
                 </div>
                 <p>Laatste 10 aankopen</p>
                 <div className={s.tablesWrapper}>
                     <div>
-                    <Table striped className={s.historyTable}>
-                        <thead><tr><th>Aanbieder: {HistoryProps.Name}</th></tr></thead>
-                        <tbody>
-                        {companyData?.recentSales.map((item, index) => (
-                            <tr key={index}>
-                                <td>
+                        <Table striped className={s.historyTable}>
+                            <thead>
+                            <tr>
+                                <th>Aanbieder: {HistoryProps.Name}</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {companyData?.recentSales.map((item, index) => (
+                                <tr key={index}>
+                                    <td>
                                         <p>
-                                            €{item.price} per stuk - ({item.quantity}x voor €{(item.price * item.quantity).toFixed(2)}) op {new Date(item.date).toLocaleDateString()} <br/>
+                                            €{item.price} per stuk - ({item.quantity}x voor
+                                            €{(item.price * item.quantity).toFixed(2)})
+                                            op {new Date(item.date).toLocaleDateString()} <br/>
                                             <span>&nbsp;</span>
                                         </p>
 
-                                </td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </Table>
+                                    </td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </Table>
                         <p>Gemiddelde prijs: €{companyData?.avgPrice.toFixed(2)}</p>
                     </div>
                     <div>
-                    <Table striped className={s.historyTable}>
-                        <thead><tr><th>Andere aanbieders:</th></tr></thead>
-                        <tbody>
-                        {notCompanyData?.recentSales.map((item, index) => (
-                            <tr key={index}>
-                                <td>
-                                    {item ? (
+                        <Table striped className={s.historyTable}>
+                            <thead>
+                            <tr>
+                                <th>Andere aanbieders:</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {notCompanyData?.recentSales.map((item, index) => (
+                                <tr key={index}>
+                                    <td>
                                         <p>
-                                            ({item.quantity}x) - €{item.price} op {new Date(item.date).toLocaleDateString()} <br/>
+                                            ({item.quantity}x) -
+                                            €{item.price} op {new Date(item.date).toLocaleDateString()} <br/>
                                             Bedrijf: {item.companyName}
                                         </p>
-                                    ) : <span>&nbsp;</span>}
-                                </td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </Table>
+                                    </td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </Table>
                         <p>Gemiddelde prijs: €{notCompanyData?.avgPrice.toFixed(2)}</p>
                     </div>
                 </div>
